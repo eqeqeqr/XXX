74172c02b0de58c0cddc493b3cf71b9d5fd0e2a7
1500292505 <1500292505@qq.com>
2023-12-09 21:01:31 +0800

    all

---
+++ src/main/java/com/xxx/kkk/HHH.java
@@ -0,0 +1,7 @@
+package com.xxx.kkk;
+
+public class HHH {
+    public void show()
+    {
+        System.out.println("成功成功成功成功成功");
+    }}
---
+++ src/test/java/com/xxx/kkk/KkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxx.kkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
